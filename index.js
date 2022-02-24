f2f2caec13f80136a2767f5592c8269b472a4923
Greg <60129025+greg0rys@users.noreply.github.com>
2022-02-24 12:34:52 -0800

    some changes

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ back.addEventListener("click", () => {
     window.history.go(-1);
     alert("clicked");
 }, true)
+
+function changeTables(table) {
+    if (!table) {
+        return 0;
+    } else {
+        return 1;
+    }
+}
