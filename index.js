423e04de39e59ca00a546fd5c002cc1ca3357b5c
Greg <60129025+greg0rys@users.noreply.github.com>
2022-02-24 13:31:05 -0800

    adding realtime clock

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-var back = document.querySelector("#back");
+var clock = document.querySelector("span#clock");
 
-back.addEventListener("click", () => {
-    window.history.go(-1);
-    alert("clicked");
-}, true)
-
-function changeTables(table) {
-    if (!table) {
-        return 0;
-    } else {
-        return 1;
-    }
+function getCurrentTime() {
+    const currentTime = new Date();
+    let h = currentTime.getHours();
+    let m = currentTime.getMinutes();
+    let s = currentTime.getSeconds();
+    h = checkHours(h);
+    m = checkTime(m);
+    s = checkTime(s);
+    clock.innerHTML = h + ":" + m + ":" + s; // throw them in our root element
+    setTimeout(getCurrentTime, 1000); // have this function call itself each second so the clock looks real.
 }
+
+function checkTime(i) {
+    return i < 10 ? (i += "0" + i) : i;
+}
+
+function checkHours(i) {
+    return i > 12 ? i - 12 : i;
+}
+
+// check if the seconds needs a 0 appended in front of it
+getCurrentTime();
